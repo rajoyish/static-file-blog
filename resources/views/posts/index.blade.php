c8197b29b08272844744f8b3899bca199737f4ad
Rajesh Budhathoki <rajoyish@gmail.com>
2023-09-30 19:56:42 +0545

    Listing blog posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,3 +1,19 @@
  <x-app-layout>
-   Index
+     <div class="space-y-16">
+        @if ($posts->count()) 
+           @foreach ($posts as $post) 
+            <article class="prose">
+                  <h1>
+                      <a href="" class="hover:text-blue-500 
+                      transition-colors duration-100"> 
+                      {{ $post->slug }}
+                     </a> 
+                  </h1>
+                  <div>teaser</div> 
+            </article>
+           @endforeach
+        @else 
+        <p>Nothing here yet.</p> 
+        @endif
+     </div>
  </x-app-layout>
